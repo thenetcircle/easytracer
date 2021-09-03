7c015b0f39f97c87bcd02f587cc6f121a2cd4761
Oscar Eriksson <oscar@thenetcircle.com>
2021-09-03 14:22:20 +0800

    class for http headers

--- easytracer/__init__.py
+++ easytracer/__init__.py
@@ -2,18 +2,24 @@ from easytracer.tracer import Span
 from easytracer.tracer import Tracer
 
 
+class HttpHeaders:
+    TRACE_ID = "x-tracer-traceid"
+    SPAN_ID = "x-tracer-spanid"
+    SERVICE_NAME = "x-tracer-servicename"
+
+
 def inject(span, carrier: dict):
-    carrier["x-tracer-traceid"] = span.trace_id
-    carrier["x-tracer-spanid"] = span.span_id
-    carrier["x-tracer-servicename"] = span.service_name
+    carrier[HttpHeaders.TRACE_ID] = span.trace_id
+    carrier[HttpHeaders.SPAN_ID] = span.span_id
+    carrier[HttpHeaders.SERVICE_NAME] = span.service_name
 
 
 def extract(headers: dict) -> Span:
     # TODO: check if headers exist
 
     return Span(
-        service_name=headers.get("x-tracer-servicename"),
+        service_name=headers.get(HttpHeaders.SERVICE_NAME),
         name="parsed",
-        trace_id=headers.get("x-tracer-traceid"),
-        span_id=headers.get("x-tracer-spanid")
+        trace_id=headers.get(HttpHeaders.TRACE_ID),
+        span_id=headers.get(HttpHeaders.SPAN_ID)
     )
