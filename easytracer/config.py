9f0ab5d8970e1490c6e3a4ab0fc414b78b09dc3b
Oscar Eriksson <oscar@thenetcircle.com>
2021-09-24 16:21:57 +0800

    more config keys

--- easytracer/config.py
+++ easytracer/config.py
@@ -4,13 +4,16 @@ from easytracer.tracer import Tracer
 class ConfigKeys:
     REPORT_HOST = "reporter_host"
     REPORT_PORT = "reporter_port"
+    LOGGING = "logging"
+    SAMPLER = "sampler"
+    PARAM = "param"
 
 
 class Config:
     def __init__(self, service_name: str, config: dict):
         self.service_name = service_name
-        self.logging = config.get("logging", True)
-        self.sampler = config.get("sampler", dict()).get("param", 1)
+        self.logging = config.get(ConfigKeys.LOGGING, True)
+        self.sampler = config.get(ConfigKeys.SAMPLER, dict()).get(ConfigKeys.PARAM, 1)
         self.config = config
 
     def init_tracer(self) -> Tracer:
