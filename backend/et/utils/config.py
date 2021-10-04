32de1f41308ad655008aee33a9775302b34b9cc5
Oscar Eriksson <oscar@thenetcircle.com>
2021-10-04 09:29:39 +0800

    no redis

--- backend/et/utils/config.py
+++ backend/et/utils/config.py
@@ -1,14 +1,11 @@
 class ConfigKeys:
     BIND_SOCKET = "bind_socket"
 
-    REDIS = "redis"
     STORAGE = "storage"
     KEY_SPACE = "key_space"
     USERNAME = "username"
     PASSWORD = "password"
     HOST = "host"
-    PORT = "port"
-    DB = "db"
 
     COLLECTOR_ENDPOINT = "collector_endpoint"
     DEFAULT_COLLECTOR_ENDPOINT = "http://127.0.0.1:6790/v1/collect"
