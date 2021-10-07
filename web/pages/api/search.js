5fcd94f74c8d7edab369e78f70b72a1c4c1b907c
Oscar Eriksson <oscar@thenetcircle.com>
2021-10-07 19:06:35 +0800

    missing commits since no autosave

--- web/pages/api/search.js
+++ web/pages/api/search.js
@@ -1,17 +1,11 @@
 import { get } from 'axios';
 
 export default (req, res) => {
-    console.log('request', req.query.q);
-    console.log('process.env.API_ENDPOINT:', process.env.API_ENDPOINT)
-    //return res.status(200).json({"status":"ok"});
-
     return get(`${process.env.API_ENDPOINT}/v1/event/${req.query.q}/spans`)
         .then(function (response) {
-            console.log(`response from server: ${JSON.stringify(response.data)}`);
             res.status(200).json(response.data);
         })
         .catch(function (error) {
-            console.log(`error calling api: ${error}`);
             res.status(400).json(error);
         })
         .then(function () {
--- web/postcss.config.js
+++ web/postcss.config.js
@@ -4,16 +4,16 @@ module.exports = {
     'autoprefixer',
     'postcss-flexbugs-fixes',
     [
-        'postcss-preset-env',
-        {
-            autoprefixer: {
-                flexbox: 'no-2009'
-            },
-            stage: 3,
-            features: {
-                'custom-properties': false
-            }
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
         }
+      }
     ]
   ],
 }
